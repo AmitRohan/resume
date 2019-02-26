5da3931b6df354228386c3b37503c3632b1f38f8
Amit Rohan <amit.rohan@juspay.in>
2019-02-26 12:39:22 +0530

    Added direct push to prod

--- updateBundle.sh
+++ updateBundle.sh
@@ -7,4 +7,9 @@ npm run compile
 cp dist/index_bundle.js ../git-resume-prod
 cd ..
 cd git-resume-prod
-# git add index_bundle.js
+git add index_bundle.js
+read -p "Commit Message [New Patch]: " commitMessage
+commitMessage=${commitMessage:-New Patch}
+git commit -m $commitMessage
+git push origin gh-pages
+echo $commitMessage
